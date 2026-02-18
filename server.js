a426bf6ef7b3760ecd011b66cd3e8dd95e175f0a
luisabianca <luisabianca93@gmail.com>
2026-02-18 12:37:48 -0300

    adjust body

--- server.js
+++ server.js
@@ -1,66 +1,70 @@
-import express from 'express'
-import { PrismaClient } from '@prisma/client';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.post("/usuarios", async (req, res) => {
-  await prisma.user.create({
+  const user = await prisma.user.create({
     data: {
       email: req.body.email,
       name: req.body.name,
-      age: req.body.age
-    }
-  })
+      age: req.body.age,
+    },
+  });
 
-  res.status(201).json(req.body)
-})
+  res.status(201).json(user);
+});
 
 app.get("/usuarios", async (req, res) => {
-  let users = [];
+  const where = {};
 
-  if(req.params) {
-    users = await prisma.user.findMany({
-      where: {
-        name: req.query.name,
-        email: req.query.email,
-        age: Number(req.query.age)
-      }
-    })
-  } else {
-    users = await prisma.user.findMany();
+  if (req.query.name) {
+    where.name = req.query.name;
   }
 
-  res.status(200).json(users)
-})
+  if (req.query.email) {
+    where.email = req.query.email;
+  }
+
+  if (req.query.age) {
+    where.age = Number(req.query.age);
+  }
+
+  const users = await prisma.user.findMany({
+    where: where,
+  });
+
+  res.status(200).json(users);
+});
 
 app.put("/usuarios/:id", async (req, res) => {
   await prisma.user.update({
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
     data: {
       email: req.body.email,
       name: req.body.name,
-      age: req.body.age
-    }
-  })
+      age: req.body.age,
+    },
+  });
 
-  res.status(201).json(req.body)
-})
+  res.status(201).json(req.body);
+});
 
 app.delete("/usuarios/:id", async (req, res) => {
   await prisma.user.delete({
     where: {
-      id: req.params.id
-    }
-  })
+      id: req.params.id,
+    },
+  });
 
-  res.status(200).json({ message: "Usuário deletado com sucesso!" })
-})
+  res.status(200).json({ message: "Usuário deletado com sucesso!" });
+});
 
-app.listen(3000)
-
+app.listen(3000);
